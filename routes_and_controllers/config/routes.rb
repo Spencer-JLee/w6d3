4584dd8e53e63e5462daaa036dc308c7dd948eb9
App Academy Student <email@email.com>
2022-03-09 16:15:52 -0500

    Create artwork_share routes

--- routes_and_controllers/config/routes.rb
+++ routes_and_controllers/config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :create, :show, :update, :destroy]
 
+  resources :artworks, only: [:index, :create, :show, :update, :destroy]
+
+  resources :artwork_shares, only: [:create, :destroy]
+
+  
   # get "/users/", to: "users#index"
   # get "/users/new", to: "users#new", as: 'new_user'
   # post "/users/", to: "users#create"
