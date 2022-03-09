4433d5423b6bf13a004c97585ee947f7cd85c69f
App Academy Student <email@email.com>
2022-03-09 15:12:46 -0500

    started phase II

--- routes_and_controllers/app/controllers/users_controller.rb
+++ routes_and_controllers/app/controllers/users_controller.rb
@@ -30,10 +30,11 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
+    render json: @user
   end
 
   private
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:username)
   end
 end
--- routes_and_controllers/db/seeds.rb
+++ routes_and_controllers/db/seeds.rb
@@ -5,8 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.destory_all
+ArtworkShare.destroy_all
 Artwork.destroy_all
+User.destroy_all
+
+
 
 users = User.create([{ username: "Steve" }, { username: "Joe" }, { username: "XxShadowlordxX" },
 { username: "DemonSniper" }, { username: "VegansVsMeatheads" }])
@@ -14,4 +17,27 @@ users = User.create([{ username: "Steve" }, { username: "Joe" }, { username: "Xx
 #  title      :string          
 #  image_url  :string        
 #  artist_id  :integer 
-artworks = Artwork.create([ { title: , image_url: , artist_id: }, ])
+artworks = Artwork.create([ 
+  { title: "Starry Night", image_url:  "flickr.com/1", artist_id: users[0].id}, 
+  { title: "Mona Lisa", image_url:  "flickr.com/2", artist_id: users[1].id},
+  { title: "Falling Water", image_url:  "flickr.com/3", artist_id: users[2].id},
+  { title: "Venus on a Seashell", image_url:  "flickr.com/4", artist_id: users[3].id},
+  { title: "Scream", image_url:  "flickr.com/5", artist_id: users[4].id}])
+
+artwork_shares = ArtworkShare.create([
+  { artwork_id: artworks[0].id, viewer_id: users[0].id},
+  { artwork_id: artworks[0].id, viewer_id: users[1].id},
+  { artwork_id: artworks[0].id, viewer_id: users[2].id},
+  { artwork_id: artworks[0].id, viewer_id: users[3].id},
+  { artwork_id: artworks[0].id, viewer_id: users[4].id},
+  { artwork_id: artworks[1].id, viewer_id: users[0].id},
+  { artwork_id: artworks[1].id, viewer_id: users[1].id},
+  { artwork_id: artworks[1].id, viewer_id: users[2].id},
+  { artwork_id: artworks[2].id, viewer_id: users[4].id},
+  { artwork_id: artworks[2].id, viewer_id: users[1].id},
+  { artwork_id: artworks[2].id, viewer_id: users[3].id},
+  { artwork_id: artworks[3].id, viewer_id: users[0].id},
+  { artwork_id: artworks[3].id, viewer_id: users[1].id},
+  { artwork_id: artworks[3].id, viewer_id: users[2].id},
+  { artwork_id: artworks[4].id, viewer_id: users[0].id}
+])
