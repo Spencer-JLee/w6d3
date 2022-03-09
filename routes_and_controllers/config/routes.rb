bed1b015fd09feaac76926e513eccd7a0074a0d7
App Academy Student <email@email.com>
2022-03-09 15:30:25 -0500

    finish artwork controller

--- routes_and_controllers/config/routes.rb
+++ routes_and_controllers/config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # resources :users
+  resources :users, only: [:index, :create, :show, :update, :destroy]
 
-  get "/users/", to: "users#index"
-  get "/users/new", to: "users#new", as: 'new_user'
-  post "/users/", to: "users#create"
-  get "/users/:id/edit", to: "users#edit", as: 'edit_user'
-  patch "/users/:id", to: "users#update"
-  put "/users/:id", to: "users#update"
-  delete "/users/:id", to: "users#destroy"
-  get "/users/:id", to: "users#show", as: 'user'
+  # get "/users/", to: "users#index"
+  # get "/users/new", to: "users#new", as: 'new_user'
+  # post "/users/", to: "users#create"
+  # get "/users/:id/edit", to: "users#edit", as: 'edit_user'
+  # patch "/users/:id", to: "users#update"
+  # put "/users/:id", to: "users#update"
+  # delete "/users/:id", to: "users#destroy"
+  # get "/users/:id", to: "users#show", as: 'user'
 end
