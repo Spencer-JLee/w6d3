9b7fe61134a38860513d56c20218be29b4af7651
App Academy Student <email@email.com>
2022-03-09 14:07:02 -0500

    created show

--- routes_and_controllers/app/controllers/users_controller.rb
+++ routes_and_controllers/app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
   def index
-    render 
+    render json: User.all
   end
 
   def create
-    render json: params
+    user = User.new(params.require(:user).permit(:name, :email))
+    if user.save
+      render json: user
+    else 
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def show 
-    render json: params
+    user = User.find(params[:id])
+    render json: user
   end
 end
